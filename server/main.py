60a4788b032893efe2e39e36b3cf920ed18a451a
3ak <jakneeley@outlook.com>
2024-04-18 15:08:48 -0500

    output sql data as json

--- server/main.py
+++ server/main.py
@@ -1,17 +1,13 @@
 from flask import Flask, redirect ,url_for, json
 from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
-from utils.dbController import initDB
+from utils.initApp import app
+from utils.dbController import initDB, deleteUser, displayAll
 
-app=Flask(__name__)
 CORS(app, resources={r"/attendees/": {"origins": "*"}})
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)    
-#db.init_app(app)
-
-initDB(app,db)
+with app.app_context():
+        initDB()
+        print(displayAll())
 
 @app.route("/")
 def index():
@@ -19,11 +15,16 @@ def index():
 
 @app.route("/attendees/", methods=['GET'])
 def attendees():
-        return "<html>Attendees</html>"
+        return displayAll()
     
-# @app.route("/attendees/add", methods=['POST'])
+# @app.route("/attendees/add/", methods=['POST'])
 # def add_attendees():
 #         return 
+
+@app.route("/attendees/del/", methods=['GET'])
+def del_attendees():
+        deleteUser(1)
+        return redirect(url_for('attendees'))
 
 if __name__ == "__main__":
         app.run(debug=True)
--- server/utils/dbController.py
+++ server/utils/dbController.py
@@ -1,39 +1,63 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import exists
+from marshmallow import Schema, fields
+import os, json
+from .initApp import db
 
-def initDB(app,db):
-    with app.app_context():
-        class User(db.Model):
-            _id = db.Column("id", db.Integer, primary_key = True)
-            email = db.Column(db.String(100), unique = True)
-            parentName = db.Column(db.String(100))
-            phone = db.Column(db.String(12))
-            childName = db.Column(db.String(100))
-            childAge = db.Column(db.Integer)
+class User(db.Model):
+    _id = db.Column("id", db.Integer, primary_key = True)
+    email = db.Column(db.String(100), unique = True)
+    parentName = db.Column(db.String(100))
+    phone = db.Column(db.String(12))
+    childName = db.Column(db.String(100))
+    childAge = db.Column(db.Integer)
 
-            def __init__(self, email, parentName, phone, childName, childAge):
-                self.email = email
-                self.parentName = parentName
-                self.phone = phone
-                self.childName = childName
-                self.childAge = childAge
-                
-            def __str__(self):
-                return "<User(email='%s', parentName='%s', phone='%s', childName='%s', childAge='%s')>" % (
-                    self.email, self.parentName, self.phone, self.childName, self.childAge)
+    def __init__(self, email, parentName, phone, childName, childAge):
+        self.email = email
+        self.parentName = parentName
+        self.phone = phone
+        self.childName = childName
+        self.childAge = childAge
         
-        #create user and insert into table
+    def __repr__(self):
+        return "<User: (email='%s', parentName='%s', phone='%s', childName='%s', childAge='%s')>" % (
+            self.email, self.parentName, self.phone, self.childName, self.childAge)
+
+
+class UserSchema(Schema):
+    _id = fields.Integer()
+    email = fields.String()
+    parentName = fields.String()
+    phone = fields.String()
+    childName = fields.String()
+    childAge = fields.Integer()
+
+
+def initDB():
+    #create table
+    db.create_all()
         
-        db.create_all()
-        
+    #check if admin user already exists, if not add admin
+    if db.session.query(exists().where(User.email == "admin@test.com")).scalar() is False:
+        print("setting up DB...")
         usr = User("admin@test.com", "Admin", "999-999-9988", "Jacob Neeley", 23)
 
         db.session.add(usr)
         db.session.commit()
+        
+        print("Done.")
+    else:
+        print("db found!")
 
-        q = db.session.execute(db.select(User).order_by(User._id)).scalars().first()
-        print(q)
 
-        delUser = db.get_or_404(User,1)
-        db.session.delete(delUser)
-        db.session.commit()
+def deleteUser(id):
+    delUser = db.get_or_404(User,id)
+    db.session.delete(delUser)
+    db.session.commit()
+    print("User has been permanently deleted.")
+
+   
+def displayAll():
+    user_schema = UserSchema()
+    user_data = db.session.query(User).all()
+    json_output = json.loads(user_schema.dumps(user_data, many=True))
+    return json_output
